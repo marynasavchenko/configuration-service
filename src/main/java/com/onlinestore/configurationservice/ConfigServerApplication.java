03e89d6ca0ab3c5418bb6cf24b7fafd75980e356
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-16 07:35:46 +0100

    added doc comments

--- src/main/java/com/onlinestore/configurationservice/ConfigServerApplication.java
+++ src/main/java/com/onlinestore/configurationservice/ConfigServerApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Class that bootstraps this service.
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
-
-	public static void main(String [] args) {
+	/**
+	 * Main method, used to run this application.
+	 *
+	 * @param args the string array, that contains command line arguments.
+	 */
+	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
