5aaa9b8cf50db167e6ba7efc0f3b4e85bd4b24df
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-04-08 16:27:12 +0200

    added @EnableEurekaClient

--- src/main/java/com/onlinestore/configurationservice/ConfigServerApplication.java
+++ src/main/java/com/onlinestore/configurationservice/ConfigServerApplication.java
@@ -3,11 +3,13 @@ package com.onlinestore.configurationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Class that bootstraps this service.
  */
 @SpringBootApplication
+@EnableEurekaClient
 @EnableConfigServer
 public class ConfigServerApplication {
 	/**
