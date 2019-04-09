9e8db2fc27f270331b536d0515e7374c09d9d1fe
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-04-09 09:16:28 +0200

    modified run.sh

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 echo "********************************************************"
+echo "Waiting for the eureka server to start  on port $EUREKASERVER_PORT"
+echo "********************************************************"
+while ! `nc -z eurekaserver $EUREKASERVER_PORT`; do sleep 3; done
+echo ">>>>>>>>>>>> Eureka Server has started"
+
+echo "********************************************************"
 echo "Starting Configuration Server"
 echo "********************************************************"
-java -jar /usr/local/configserver/@project.build.finalName@.jar
+java -Djava.security.egd=file:/dev/./urandom -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI -jar /usr/local/configserver/@project.build.finalName@.jar
